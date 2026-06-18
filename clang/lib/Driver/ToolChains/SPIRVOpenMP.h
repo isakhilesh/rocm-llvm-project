1882a6a8ccbebf5faced05f9b614ddec01913a93
Akhilesh Moorthy <akhilesh.moorthy@amd.com>
2026-06-18 14:04:16 -0500

    Corrections

--- clang/lib/Driver/ToolChains/SPIRVOpenMP.h
+++ clang/lib/Driver/ToolChains/SPIRVOpenMP.h
@@ -1,4 +1,4 @@
-//===--- SPIRVOpenMP.h - SPIR-V OpenMP ToolChain ----------------*- C++ -*-===//
+//===--- SPIRVOpenMP.h - SPIR-V OpenMP Tool Implementations ------*- C++-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
